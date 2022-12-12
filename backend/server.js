f4047bcb853d40e9c7b72de5542ead716fad0ac5
DavidKocsis0204 <kocsisdavid20020204@gmail.com>
2022-12-12 11:11:02 +0100

    config.js hozzáadva

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,20 @@ const cors = require('cors');
 const multer = require('multer');
 const app = express();
 const version = process.env.VERSION;
+const config = require('./config.js');
 
 app.get('/', (req, res) => {
     log(req.socket.remoteAddress, `Sent version information.`);
     res.status(200).send(`2/14.szft Backend API ${version}.`);
 });
+
+
+//Middlewares
+app.use('/assets',express.static(path.join(__dirname, '/assets')));
+app.use('/views',express.static(path.join(__dirname, '/views')));
+app.use(express.urlencoded({ extended: true }));
+
+
+app.listen(config.appconfig.port, ()=>{
+    console.log(`Server is listening on port ${config.appconfig.port}...`);
+})
