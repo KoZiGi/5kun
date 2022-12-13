b67b7a273bb55542b8d1080506bea46d0248cd58
ginalzsolt <ginalzsolt@turr.hu>
2022-12-13 12:22:26 +0100

    registration donw

--- frontend/controllers/regCtrl.js
+++ frontend/controllers/regCtrl.js
@@ -1,27 +1,60 @@
 app.controller('regCtrl', function($scope,$rootScope,db) {
     $scope.newuser={}
-    if($scope.newuser.name==""|| $scope.newuser.email==""||$scope.newuser.password==""||$scope.newuser.pw2==""){
-        //TODO: Nincs kitöltve minden
-    }
-    else{
-        if($scope.newuser.password!=$scope.newuser.pw2){
-            //TODO: Nem egyeznek a jelszavak
+    $scope.error = {};
+    $scope.ShutUp = function(){$scope.error = false;}
+    $scope.Register= function(){
+        if($scope.newuser.name==""|| $scope.newuser.email==""||$scope.newuser.password==""||$scope.newuser.pw2==""){
+            $scope.error = {
+                show: true,
+                type: 'danger',
+                message: 'Empty fields are not allowed!'
+            }
         }
         else{
-            if(!$scope.newuser.password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$")){
-                //TODO: A jelszónak tartalmaznia kell kis-és nagybetűt valamint számot 
+            if($scope.newuser.password!=$scope.newuser.pw2){
+                $scope.error = {
+                    show: true,
+                    type: 'danger',
+                    message: 'The two passwords dont match!'
+                }
             }
             else{
-                db.select("user","email",$scope.newuser.email).then(function(results){
-                    if(results.length!=0){
-                        //TODO: Ez az Email már foglalt
+                if(!$scope.newuser.password.matches("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])$")){
+                    $scope.error = {
+                        show: true,
+                        type: 'danger',
+                        message: 'The password has to contain at least one lowercase, one uppercase character, and a number!'
+                    }
+                }
+                else{
+                    if($scope.newuser.password.length<8){
+                        $scope.error = {
+                            show: true,
+                            type: 'danger',
+                            message: 'The password has to be at least eight characters long!'
+                        }
                     }
                     else{
-                        db.insert("users",$scope.newuser).then(function(){
-                            //TODO: Sikeres regisztráció
+                        db.select("user","email",$scope.newuser.email).then(function(results){
+                            if(results.length!=0){
+                                $scope.error = {
+                                    show: true,
+                                    type: 'danger',
+                                    message: 'This Email is already taken!'
+                                }
+                            }
+                            else{
+                                db.insert("users",$scope.newuser).then(function(){
+                                    $scope.error = {
+                                        show: true,
+                                        type: 'success',
+                                        message: 'Registration successfull!'
+                                    }
+                                })
+                            }
                         })
                     }
-                })
+                }
             }
         }
     }
