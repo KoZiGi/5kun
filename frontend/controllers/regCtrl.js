238e417ec32e97335273fcc7ac96eac14e98707f
ginalzsolt <ginalzsolt@turr.hu>
2022-12-13 12:00:02 +0100

    db change

--- frontend/controllers/regCtrl.js
+++ frontend/controllers/regCtrl.js
@@ -1,3 +1,19 @@
-app.controller('regCtrl', function($scope,$rootScope,) {
-    
+app.controller('regCtrl', function($scope,$rootScope,db) {
+    $scope.newuser={}
+    if($scope.newuser.name==""|| $scope.newuser.email==""||$scope.newuser.pw1==""||$scope.newuser.pw2==""){
+        //TODO: nincs kitöltve minden
+    }
+    else{
+        if($scope.newuser.pw1!=$scope.newuser.pw2){
+            //TODO: nem egyeznek a jelszavak
+        }
+        else{
+            if(!$scope.newuser.pw1.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$")){
+                //TODO: a jelszónak tartalmaznia kell kis-és nagybetűt valamint számot 
+            }
+            else{
+                db.select
+            }
+        }
+    }
 })
