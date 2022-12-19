b48ad0f3b28d649b822ddc39d5c7bd31e89c16de
zickbalazs <zickbalazs@turr.hu>
2022-12-19 09:56:50 +0100

    Posztok létrehozása megoldva

--- frontend/controllers/feedCtrl.js
+++ frontend/controllers/feedCtrl.js
@@ -1,3 +1,40 @@
 app.controller('feedCtrl', function($rootScope, $scope, db){
     $scope.newpost = {};
+    $scope.message = {
+        text: "",
+        icon: "",
+        type: "",
+        show: false
+    };
+    $scope.posts = [];
+    db.selectAll('postView').then(function(res){
+        $scope.posts = res.data;
+        console.log($scope.posts);
+    })
+
+    $scope.Post = function(){
+        let data = {
+            userID: $rootScope.user.ID,
+            postmessage: $scope.newpost.postmessage
+        }
+        db.insert('posts', data).then(function(res){
+            if (res.status==200){
+                if (res.data.insertId){
+                    $scope.setMessage('success', 'check-lg', 'Successful post!')
+                }
+            }
+            else{
+                $scope.setMessage('danger', 'exclamation-circle', 'Unuccessful post!')
+            }
+        });
+    }
+    $scope.setMessage = function(type, icon, text){
+        $scope.message.text = text;
+        $scope.message.icon = icon;
+        $scope.message.type = type;
+        $scope.message.show = true;
+    }
 })
+
+
+
